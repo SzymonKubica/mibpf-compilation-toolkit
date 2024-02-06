94e8cc3f45c60ff3003c72cb519b7db659623fe4
SzymonKubica <szymo.kubica@gmail.com>
2024-02-06 11:30:56 +0000

    Updated the bytecode patching script.

--- scripts/adjust-bytecode.sh
+++ scripts/adjust-bytecode.sh
@@ -10,17 +10,16 @@
 #
 # TODO: make this work for all programs
 # Investigate: is this actually needed for all memory loads?
+# Check: how is it actually done in the linux kernel?
+# Reach out to Quentin.
 
-#xxd $1 | tee checkpoint | sed ' s/6112 5000 0000 0000/7912 5000 0000 0000/ ;
-#    s/6111 4c00 0000 0000/7911 4000 0000 0000/ ;
-#   s/6111 2200 0000 0000/7911 2200 0000 0000/' | xxd -r > $1.tmp
-# Testing different program
 # Effectively all instructions starting with 61 need to be replaced by 79
 xxd $1 | tee checkpoint | sed ' s/6112 5000 0000 0000/7912 5000 0000 0000/ ;
     s/6111 4c00 0000 0000/7911 4000 0000 0000/ ;
     s/6116 4c00 0000 0000/7916 4000 0000 0000/ ;
     s/6117 4c00 0000 0000/7917 4000 0000 0000/ ;
     s/6111 5000 0000 0000/7911 5000 0000 0000/ ;
+    s/6112 4c00 0000 0000/7912 4000 0000 0000/ ;
    s/6111 2200 0000 0000/7911 2200 0000 0000/' | xxd -r > $1.tmp
 
 mv $1.tmp $1
