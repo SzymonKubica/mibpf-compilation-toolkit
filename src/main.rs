1dd4acb5e295bad97eccb0af0f91c6d053217d5a
SzymonKubica <szymo.kubica@gmail.com>
2024-02-07 18:13:13 +0100

    Added proper cli args parsing with clap with derive features.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,43 @@ use std::fs::File;
 use std::io::Write;
 use std::{path::PathBuf, process::Command};
 
+use clap::Parser;
+mod args;
+mod compile;
+mod execute;
+mod pull;
+mod sign;
+
+extern crate clap;
 extern crate rbpf;
 use rbpf::helpers;
 
+use crate::compile::{handle_compile, VmTarget};
+
 fn main() {
-    let file_name = std::env::args().nth(1).unwrap();
+    let args = args::Args::parse();
+
+    match &args.command {
+        args::Action::Compile {
+            bpf_source_file,
+            target,
+            output_file,
+        } => {
+            let vm_target = VmTarget::from(target.clone());
+            handle_compile(bpf_source_file, vm_target, output_file)
+        }
+
+        args::Action::Sign {
+            host_network_interface,
+            board_name,
+            coaproot_dir,
+            binary_name,
+        } => {
+        }
+        args::Action::Pull {} => todo!(),
+        args::Action::Execute {} => todo!(),
+    }
+    let file_name = "test";
 
     let base_name = file_name
         .split("/")
