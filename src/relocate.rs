691244d678a1b07b6d42f59e7930b22140471f1e
SzymonKubica <szymo.kubica@gmail.com>
2024-03-03 23:47:05 +0000

    Added basic relocation implementation equivalent to the rbpf script.

--- src/relocate.rs
+++ src/relocate.rs
@@ -3,7 +3,7 @@ use std::{
     io::Read,
 };
 
-use goblin::elf64::sym::{STB_GLOBAL, STT_FUNC, STT_SECTION};
+use goblin::elf64::sym::{STB_GLOBAL, STT_FUNC, STT_OBJECT, STT_SECTION};
 
 use crate::args::Action;
 
@@ -111,6 +111,8 @@ pub fn handle_relocate(args: &crate::args::Action) {
                 }
             }
 
+            let mut symbol_structs = vec![];
+
             // No we append all relocation symbols to the rodata section,
             for (name, text_offset) in symbol_map {
                 let offset = rodata.len();
@@ -120,6 +122,10 @@ pub fn handle_relocate(args: &crate::args::Action) {
                     "Symbol {} with offset {} appended at {}",
                     name, text_offset, offset
                 );
+                // Added flags for compatiblity with rbpf
+                let flags = 0;
+                symbol_structs.push((offset, flags, text_offset));
+
             }
 
             // Handle relocations
@@ -155,48 +161,89 @@ pub fn handle_relocate(args: &crate::args::Action) {
                             }
                         }
 
-                        patch_text(&mut text, &binary, reloc, str_section_offsets);
+                        patch_text(&mut text, &binary, reloc, &str_section_offsets);
                     }
                 }
             }
+        print_bytes(&text);
         }
 
-        /*
 
-        // Now handle relocations
-        /*
-        *         if relocations:
-           for relocation in relocations.iter_relocations():
-               logging.debug(relocation.entry)
-               entry = relocation.entry
-               symbol = symbols.get_symbol(entry['r_info_sym'])
-               if symbol.entry['st_info']['type'] == 'STT_SECTION':
-                   name = elffile.get_section(symbol.entry['st_shndx']).name
-                   logging.info(f"relocation at instruction {hex(entry['r_offset'])} for section {name} at offset {symbol.entry.st_value}")
-               else:
-                   name = symbol.name
-                   section = elffile.get_section(symbol.entry.st_shndx)
-                   logging.info(f"relocation at instruction {hex(entry['r_offset'])} for symbol {name} in {section.name} at {symbol.entry.st_value}")
-
-        */
-
-        if let Some(relocations) = relocations {
-            println!("Relocations: {}", relocations);
-            let mut offset;
-            elf::relocation::Rel::parse_at(BigEndian, Class::ELF32, &mut offset, relocations.data);
-        }
-        */
     } else {
         panic!("Invalid action args: {:?}", args);
     }
 }
 
+#[repr(C, packed)]
+struct Lddw {
+    opcode: u8,
+    registers: u8,
+    offset: u16,
+    immediate_l: u32,
+    null1: u8,
+    null2: u8,
+    null3: u16,
+    immediate_h: u32,
+}
+
 fn patch_text(
-    text: &[u8],
+    text: &mut [u8],
     binary: &goblin::elf::Elf<'_>,
     reloc: goblin::elf::Reloc,
-    str_section_offsets: std::collections::HashMap<&str, usize>,
+    str_section_offsets: &std::collections::HashMap<&str, usize>,
 ) {
+    let symbol = binary.syms.get(reloc.r_sym).unwrap();
+    let section = binary.section_headers.get(symbol.st_shndx).unwrap();
+    let section_name = binary.strtab.get_at(section.sh_name).unwrap();
+    let mut offset = 0;
+    if symbol.st_type() == STT_SECTION {
+        println!("section_name: {}", section_name);
+        println!("registered section offsets: {:?}", str_section_offsets);
+        if let Some(off) = str_section_offsets.get(section_name) {
+            offset = *off;
+        } else {
+            return;
+        }
+    } else if symbol.st_type() == STT_OBJECT {
+        offset = symbol.st_value as usize;
+    } else if symbol.st_type() == STT_FUNC {
+        // We don't do eny relocations in case of functions as they are handled
+        // in a custom way by the VM (we append their relocation structs at the end of the binary
+        // file)
+        return;
+    }
+
+    const LDDWD_OPCODE: u32 = 0xB8;
+    const LDDWR_OPCODE: u32 = 0xD8;
+    const LDDW_OPCODE: u32 = 0x18;
+
+    let opcode = if section_name.contains(".rodata.str") {
+        LDDWR_OPCODE
+    } else {
+        LDDWD_OPCODE
+    };
+
+    if text[reloc.r_offset as usize] != LDDW_OPCODE as u8 {
+        println!("No LDDW instruction at {}", reloc.r_offset);
+        return;
+    } else {
+        let instruction = Vec::from(&text[reloc.r_offset as usize..reloc.r_offset as usize + 16]);
+        println!(
+            "Replacing {:?} at {} with {} at {}",
+            instruction, reloc.r_offset, opcode, reloc.r_offset
+        );
+        //LDDW_STRUCT = struct.Struct('<BBHiBBHi')
+        let mut instr: Lddw = unsafe { std::ptr::read(instruction.as_ptr() as *const _) };
+
+        instr.opcode = opcode as u8;
+        instr.immediate_l += offset as u32;
+
+        let new_instruction =
+            unsafe { std::slice::from_raw_parts(&instr as *const _ as *const u8, 16) };
+
+        text[reloc.r_offset as usize..reloc.r_offset as usize + 16]
+            .copy_from_slice(new_instruction);
+    }
 }
 
 fn print_bytes(bytes: &[u8]) {
