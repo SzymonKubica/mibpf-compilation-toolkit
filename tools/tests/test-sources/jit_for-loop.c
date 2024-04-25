ba6d9431fdc00db03ed9389b525a1ecd192bdc02
SzymonKubica <szymo.kubica@gmail.com>
2024-04-26 00:08:20 +0200

    Tested the for loop example on 1000 iterations of calling a helper function. JIT is about 6 times faster.

--- tools/tests/test-sources/jit_for-loop.c
+++ tools/tests/test-sources/jit_for-loop.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 int helper_call(void *ctx) {
     uint32_t result = 0;
-    for (int8_t i = 0; i <= 100; i++ ) {
+    for (int16_t i = 0; i <= 1000; i++ ) {
         bpf_store_global(0, i);
     }
     bpf_fetch_global(0, &result);
