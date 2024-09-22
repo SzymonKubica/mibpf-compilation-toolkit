21314d873755814b02362945560bb4ccf2152b0d
SzymonKubica <szymo.kubica@gmail.com>
2024-09-22 10:08:46 +0100

    Fix .env parsing.

--- tools/src/environment.rs
+++ tools/src/environment.rs
@@ -1,5 +1,7 @@
 use std::{env, path::Path};
 
+use log::debug;
+
 #[derive(Debug)]
 pub struct Environment {
     /// Root directory of the mibpf repository.
@@ -23,11 +25,12 @@ pub struct Environment {
 }
 
 pub fn load_env() -> Environment {
-    let path_str = env::var("DOTENV").unwrap_or_else(|_| ".env".to_string());
+    let path_str = env::var("DOTENV").unwrap_or_else(|_| ".env-nucleo".to_string());
     let path = Path::new(&path_str);
+    debug!("Loading env from: {:?}", path);
     let _ = dotenv::from_path(path);
 
-    Environment {
+    let env = Environment {
         micro_bpf_root_dir: dotenv::var("MIBPF_ROOT_DIR").unwrap_or_else(|_| "..".to_string()),
         coap_root_dir: dotenv::var("COAP_ROOT_DIR").unwrap_or_else(|_| "../coaproot".to_string()),
         out_dir: dotenv::var("OUT_DIR").unwrap_or_else(|_| "../out".to_string()),
@@ -36,8 +39,11 @@ pub fn load_env() -> Environment {
         riot_instance_ip: dotenv::var("RIOT_INSTANCE_IP")
             .unwrap_or_else(|_| "fe80::a0d9:ebff:fed5:986b".to_string()),
         host_net_if: dotenv::var("HOST_NET_IF").unwrap_or_else(|_| "tapbr0".to_string()),
-        host_ip: dotenv::var("HOST_IP").unwrap_or_else(|_| "fe80::cc9a:73ff:fe4a:47f6".to_string()),
-        board_name: dotenv::var("BOARD_NAME").unwrap_or_else(|_| "native".to_string()),
-        src_dir: dotenv::var("SRC_DIR").unwrap_or_else(|_| "../bpf/tests".to_string())
-    }
+        host_ip: dotenv::var("HOST_IP").unwrap_or_else(|_| "bad-ip".to_string()),
+        board_name: dotenv::var("BOARD_NAME").unwrap_or_else(|_| "bad-board".to_string()),
+        src_dir: dotenv::var("SRC_DIR").unwrap_or_else(|_| "../bpf/tests".to_string()),
+    };
+
+    debug!("Loaded env: \n{:?}", env);
+    env
 }
